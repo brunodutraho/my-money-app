e7fceae2edf61e8d76e4a4f89a3e682de1d0c0e9
Bruno Dutra <brunodutra.ho@gmail.com>
2025-02-20 10:32:55 -0300

    getting the registration quantity

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -8,7 +8,7 @@ const creditSchema = new mongoose.Schema({
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    value: { type: Number, min: 0, required: [true, 'Inoforme o valor do débito!']  },
     status: { type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
 })
 
@@ -20,4 +20,4 @@ const billingCycleSchema = new mongoose.Schema({
     debts: [debtSchema]
 })
 
-module.export = restful.model('BillingCycle', billingCycleSchema)
+module.exports = restful.model('BillingCycle', billingCycleSchema)
