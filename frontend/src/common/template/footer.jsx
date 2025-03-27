ca3aacfcc77c524d18301e99a78e977aebd34d56
Bruno Dutra <brunodutra.ho@gmail.com>
2025-03-27 10:00:24 -0300

    feat(template): footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyringht &copy; 2025
+            <a href="#" target='_blank'> Bruno Dutra</a>.
+        </strong>
+    </footer>
+)
