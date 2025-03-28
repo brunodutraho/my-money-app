46b7e2b9b5e4cd1830691f2238a3fc00ff97abeb
Bruno Dutra <brunodutra.ho@gmail.com>
2025-03-28 12:54:12 -0300

    feat(src): configurando middleware

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import {  applyMiddleware ,createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
+
 import App from './main/app'
+import Reducers from './main/reducers'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+
+const store = applyMiddleware(promise)(createStore)(Reducers);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
