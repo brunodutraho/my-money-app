cb0b697d0053839093177260295c06deb25d35b8
Bruno Dutra <brunodutra.ho@gmail.com>
2025-03-29 14:29:30 -0300

    feat(src): integrando devTools

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,10 +8,12 @@ import promise from 'redux-promise'
 import App from './main/app'
 import Reducers from './main/reducers'
 
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__();
 const container = document.getElementById('root');
 const root = createRoot(container);
-
-const store = applyMiddleware(promise)(createStore)(Reducers);
+const store = applyMiddleware(promise)(createStore)(Reducers, devTools);
 root.render(
     <Provider store={store}>
         <App />
