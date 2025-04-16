cfc788afc2ddee3e84047d65d08014924caf58f2
Bruno Dutra <brunodutra.ho@gmail.com>
2025-04-15 21:57:30 -0300

    feat(index.jsx): implementação do redux-thunk

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import {  applyMiddleware ,createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
 
 import promise from 'redux-promise'
+import { thunk } from 'redux-thunk'
 
 import App from './main/app'
 import Reducers from './main/reducers'
 
-
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
-    && window.__REDUX_DEVTOOLS_EXTENSION__();
-const container = document.getElementById('root');
-const root = createRoot(container);
-const store = applyMiddleware(promise)(createStore)(Reducers, devTools);
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const container = document.getElementById('root')
+const root = createRoot(container)
+const store = applyMiddleware(thunk, promise)(createStore)(Reducers, devTools)
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-);
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
