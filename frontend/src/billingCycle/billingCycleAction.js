e0ba1ec2992c7b246bf4a1c00fae81035cf6a296
Bruno Dutra <brunodutra.ho@gmail.com>
2025-04-15 22:13:34 -0300

    feat(billingCycleAtion.js): implementação da função create com chamada async, validação utilizando react-redux-toastr.

--- frontend/src/billingCycle/billingCycleAction.js
+++ frontend/src/billingCycle/billingCycleAction.js
@@ -1,10 +1,48 @@
 import axios from 'axios'
-const BASE_URL = 'https://localhost:3003/api'
+import { toastr } from 'react-redux-toastr'
+
+const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
-    const request = axios.get(`${BASE_URL}/billingCycles`)
-    return {
-        type: 'BILLING_CYCLES_FETCHED',
-        payload: request
+  const request = axios.get(`${BASE_URL}/billingCycles`)
+  return {
+    type: 'BILLING_CYCLES_FETCHED',
+    payload: request,
+  }
+}
+
+export function create(values) {
+  return async dispatch => {
+    try {
+      // Envia os dados para a API para criar um novo ciclo de pagamento
+      const response = await axios.post(`${BASE_URL}/billingCycles`, values)
+
+      // Despacha a ação com os dados do ciclo criado
+      dispatch({
+        type: 'BILLING_CYCLE_CREATED',
+        payload: response.data,
+      })
+
+      // Atualiza a lista de ciclos
+      dispatch(getList())
+
+      // Exibe uma notificação de sucesso no toastr
+      toastr.success('Sucesso', 'Operação incluida com sucesso!')
+      // Redireciona para a página de listagem de ciclos
+    } catch (error) {
+      console.error('Erro ao criar ciclo de pagamento:', error)
+
+      // Exibe uma notificação de erro no toastr
+      if (error.response && error.response.data && error.response.data.errors) {
+        error.response.data.errors.forEach(err =>
+          toastr.error('Erro ao Criar', err)
+        )
+      } else {
+        toastr.error(
+          'Erro',
+          'Ocorreu um erro ao tentar criar o ciclo de pagamento.'
+        )
+      }
     }
+  }
 }
