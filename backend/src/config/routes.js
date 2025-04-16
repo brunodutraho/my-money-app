0f98ecc60993e222d6d344f10cb0f212a32a17a7
Bruno Dutra <brunodutra.ho@gmail.com>
2025-04-15 21:50:58 -0300

    style: formatação de routes.js no backend

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,11 @@
 const express = require('express')
 
-module.exports = (server) => {
+module.exports = server => {
+  //Definir URL base para todas as rotas
+  const router = express.Router()
+  server.use('/api', router)
 
-    //Definir URL base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router)
-
-    //Rotas de Ciclo de Pagamento
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+  //Rotas de Ciclo de Pagamento
+  const BillingCycle = require('../api/billingCycle/billingCycleService')
+  BillingCycle.register(router, '/billingCycles')
 }
