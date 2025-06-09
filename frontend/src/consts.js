a58655d7ada7227815bf6c4f37c2f949501cf971
Bruno Dutra <brunodutra.ho@gmail.com>
2025-06-09 12:16:30 -0300

    atualização consts.js

--- frontend/src/consts.js
+++ frontend/src/consts.js
@@ -1,5 +1,5 @@
-const API_URL = 'http://localhost:3003/api'
-const OAPI_URL = 'http://localhost:3003/oapi'
+const API_URL = 'https://my-money-app-yq7b.onrender.com/api'
+const OAPI_URL = 'https://my-money-app-yq7b.onrender.com/oapi'
 
 export default {
   API_URL,
